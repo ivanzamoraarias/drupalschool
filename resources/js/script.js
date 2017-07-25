917a3549be40187b7ddf4f720ed8bc40c2c2b583
ivanzamoraarias <faucar93@gmail.com>
2017-07-25 18:13:55 -0500

    javascript added

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,14 @@
 /**
  * Created by root on 7/24/17.
  */
+
+$(document).ready(function() {
+    // all custom jQuery will go here
+    //alert('holaaaa');
+    $('.white_nav_bar_li').mouseenter(function () {
+        $(this).children('.white_nav_bar_li_hide').show();
+    });
+    $('.white_nav_bar_li').mouseleave(function () {
+        $(this).children('.white_nav_bar_li_hide').hide();
+    });
+});
